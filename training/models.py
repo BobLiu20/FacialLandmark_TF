f606b5d972cd8c646ba8a3c8d81cd630727da5e1
Bob.Liu <baifang20@qq.com>
2017-11-22 09:54:38 +0800

    [feature]Add support multi gpu training

--- training/models.py
+++ training/models.py
@@ -6,58 +6,60 @@ from tensorflow.contrib.framework import arg_scope
 
 slim = tf.contrib.slim
 
+# models
 def fanet8ss_inference(inputs, num_label, **kwargs): 
-    inputs = inputs/127.5-1.0
-    h_conv1 = conv2d(inputs, 16, 3, stride=2)
+    with tf.variable_scope('fanet8ss_inference') as scope:
+        inputs = inputs/127.5-1.0
+        h_conv1 = conv2d(inputs, 16, 3, stride=2)
 
-    h_conv2 = conv2d(h_conv1, 32, 3)
-    h_conv3 = conv2d(h_conv2, 32, 3, stride=2)
+        h_conv2 = conv2d(h_conv1, 32, 3)
+        h_conv3 = conv2d(h_conv2, 32, 3, stride=2)
 
-    h_conv4 = conv2d(h_conv3, 32, 3)
-    h_conv5 = conv2d(h_conv4, 32, 3, stride=2)
+        h_conv4 = conv2d(h_conv3, 32, 3)
+        h_conv5 = conv2d(h_conv4, 32, 3, stride=2)
 
-    h_conv6 = conv2d(h_conv5, 64, 3)
-    h_conv7 = conv2d(h_conv6, 64, 3, stride=2)
+        h_conv6 = conv2d(h_conv5, 64, 3)
+        h_conv7 = conv2d(h_conv6, 64, 3, stride=2)
 
-    h_conv8 = conv2d(h_conv7, 128, 3)
-    h_pool1 = max_pool2d(h_conv8, 2, 2)
-    h_pool1_flat = flatten(h_pool1)
-    h_fc1 = fc(h_pool1_flat, 512)
-    h_fc2 = fc(h_fc1, num_label, activation_fn=tf.nn.sigmoid)
-    return h_fc2
+        h_conv8 = conv2d(h_conv7, 128, 3)
+        h_pool1 = max_pool2d(h_conv8, 2, 2)
+        h_pool1_flat = flatten(h_pool1)
+        h_fc1 = fc(h_pool1_flat, 512)
+        h_fc2 = fc(h_fc1, num_label, activation_fn=tf.nn.sigmoid)
+        return h_fc2
 
 def fanet8ss_conv_1_1_16_16_16_exp(input_tensor, num_label, **kwargs):
-    normal_tensor = input_tensor/127.5 - 1.0
-    h_conv1 = conv2d(normal_tensor, 16, 3, stride=2)
-    h_conv2 = conv2d(h_conv1, 16, 1)
-    h_conv3 = conv2d(h_conv2, 16, 3, stride=2)
-    h_conv4 = conv2d(h_conv3, 16, 1)
-    h_conv5 = conv2d(h_conv4, 32, 3, stride=2)
-    h_conv6 = conv2d(h_conv5, 64, 3)
-    h_conv7 = conv2d(h_conv6, 64, 3, stride=2)
-    h_conv8 = conv2d(h_conv7, 128, 3)
-    h_pool1 = max_pool2d(h_conv8, 2, 2)
-    h_pool1_flat = flatten(h_pool1)
-    h_fc1 = fc(h_pool1_flat, 512)
-    point = fc(h_fc1, num_label, activation_fn=None)
-    return point
+    with tf.variable_scope('fanet8ss_conv_1_1_16_16_16_exp') as scope:
+        normal_tensor = input_tensor/127.5 - 1.0
+        h_conv1 = conv2d(normal_tensor, 16, 3, stride=2)
+        h_conv2 = conv2d(h_conv1, 16, 1)
+        h_conv3 = conv2d(h_conv2, 16, 3, stride=2)
+        h_conv4 = conv2d(h_conv3, 16, 1)
+        h_conv5 = conv2d(h_conv4, 32, 3, stride=2)
+        h_conv6 = conv2d(h_conv5, 64, 3)
+        h_conv7 = conv2d(h_conv6, 64, 3, stride=2)
+        h_conv8 = conv2d(h_conv7, 128, 3)
+        h_pool1 = max_pool2d(h_conv8, 2, 2)
+        h_pool1_flat = flatten(h_pool1)
+        h_fc1 = fc(h_pool1_flat, 512)
+        point = fc(h_fc1, num_label, activation_fn=None)
+        return point
 
-def inception_v3(inputs, num_label, **kwargs):
-    from tensorflow.contrib.slim.python.slim.nets.inception_v3 import inception_v3, inception_v3_arg_scope
-    arg_scope = inception_v3_arg_scope()
-    with tf.contrib.slim.arg_scope(arg_scope):
-        logits, _ = inception_v3(inputs, num_label, is_training=kwargs['is_training'])
-    return logits
-
-def get_l2_loss(logits, labels, batch_size):
+# loss
+def get_l2_loss(logits, labels):
     loss = tf.nn.l2_loss(tf.subtract(logits, labels))
     loss = tf.truediv(loss, tf.constant(batch_size, dtype=tf.float32))
     batchnorm_updates = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     batchnorm_updates_op = tf.group(*batchnorm_updates)
     with tf.control_dependencies([batchnorm_updates_op]):
         total_loss = tf.identity(loss)
+    tf.add_to_collection(tf.GraphKeys.LOSSES, total_loss)
     return total_loss
 
+def get_mse_loss(logits, labels):
+    return tf.losses.mean_squared_error(labels, logits)
+
+# init
 def init(model, images, num_labels, **kwargs):
     func = getattr(sys.modules["models"], model)
     return func(images, num_labels, **kwargs)
--- training/training.py
+++ training/training.py
@@ -11,61 +11,124 @@ sys.path.insert(0, os.path.join(MY_DIRNAME, '..', 'common'))
 from batch_reader import BatchReader
 import models
 
+def tower_loss(scope, images, labels, model_name, num_labels):
+    # Build inference Graph.
+    logits = models.init(model_name, images, num_labels, is_training=True)
+    # Build the portion of the Graph calculating the losses. Note that we will
+    # assemble the total_loss using a custom function below.
+    _ = models.get_mse_loss(logits, labels)
+    # Assemble all of the losses for the current tower only.
+    losses = tf.get_collection(tf.GraphKeys.LOSSES, scope)
+    # Calculate the total loss for the current tower.
+    total_loss = tf.add_n(losses, name='total_loss')
+    return total_loss
+
+def average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        # Note that each grad_and_vars looks like the following:
+        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+        grads = []
+        for g, _ in grad_and_vars:
+            # Add 0 dimension to the gradients to represent the tower.
+            expanded_g = tf.expand_dims(g, 0)
+
+            # Append on a 'tower' dimension which we will average over below.
+            grads.append(expanded_g)
+
+        # Average over the 'tower' dimension.
+        grad = tf.concat(axis=0, values=grads)
+        grad = tf.reduce_mean(grad, 0)
+
+        # Keep in mind that the Variables are redundant because they are shared
+        # across towers. So .. we will just return the first tower's pointer to
+        # the Variable.
+        v = grad_and_vars[0][1]
+        grad_and_var = (grad, v)
+        average_grads.append(grad_and_var)
+    return average_grads
+
 def train(prefix, **arg_dict):
     batch_size = arg_dict['batch_size']
     num_labels = arg_dict['landmark_type'] * 2
     img_size = arg_dict['img_size']
-    # batch generator
-    _batch_reader = BatchReader(**arg_dict)
-    _batch_generator = _batch_reader.batch_generator()
+    gpu_list = map(int, arg_dict['gpu_device'].split(','))
+    assert (batch_size % len(gpu_list) == 0), "Batch size must exact division by gpu nums"
 
-    with tf.Graph().as_default():
+    with tf.Graph().as_default(), tf.device('/cpu:0'):
+        # data input
         images = tf.placeholder(tf.float32, shape=[batch_size, img_size, img_size, 3])
-        point_labels = tf.placeholder(tf.float32, shape=[batch_size, num_labels])
-
-        logits = models.init(arg_dict['model'], images, num_labels, is_training=True)
-
-        loss = models.get_l2_loss(logits, point_labels, batch_size)
-
-        # Create a variable to track the global step.
-        global_step = tf.Variable(0, name='global_step', trainable=False)
-        learning_rate = tf.train.exponential_decay(arg_dict['learning_rate'], global_step, 30000, 0.5, staircase=True)
-        # Use the optimizer to apply the gradients that minimize the loss
-        # (and also increment the global step counter) as a single training step.
-        optimizer = tf.train.AdamOptimizer(learning_rate)
-        train_op = optimizer.minimize(loss, global_step=global_step)
-
-        sess = tf.Session(config=tf.ConfigProto(
-            gpu_options = tf.GPUOptions(allow_growth=True)
-            )) 
+        labels = tf.placeholder(tf.float32, shape=[batch_size, num_labels])
+        images_split = tf.split(images, len(gpu_list), axis=0)
+        labels_split = tf.split(labels, len(gpu_list), axis=0)
+        # Create a variable to count the number of train() calls.
+        global_step = tf.get_variable(
+            'global_step', [],
+            initializer=tf.constant_initializer(0), trainable=False)
+        # Decay the learning rate exponentially based on the number of steps.
+        lr = tf.train.exponential_decay(arg_dict['learning_rate'],
+                                        global_step,
+                                        30000,
+                                        0.8,
+                                        staircase=True)
+        # Create an optimizer that performs gradient descent.
+        optimizer = tf.train.AdamOptimizer(lr)
+        # Calculate the gradients for each model tower.
+        tower_grads = []
+        with tf.variable_scope(tf.get_variable_scope()):
+            for i in xrange(len(gpu_list)):
+                with tf.device('/gpu:%d' % i):
+                    with tf.name_scope('%s_%d' % ("landmarks", i)) as scope:
+                        loss = tower_loss(scope, images_split[i], labels_split[i], arg_dict['model'], num_labels)
+                        tf.get_variable_scope().reuse_variables()
+                        # Calculate the gradients for the batch of data on this tower.
+                        grads = optimizer.compute_gradients(loss)
+                        # Keep track of the gradients across all towers.
+                        tower_grads.append(grads)
+        # We must calculate the mean of each gradient. Note that this is the
+        # synchronization point across all towers.
+        grads = average_gradients(tower_grads)
+        # Apply the gradients to adjust the shared variables.
+        apply_gradient_op = optimizer.apply_gradients(grads, global_step=global_step)
+        # Track the moving averages of all trainable variables.
+        variable_averages = tf.train.ExponentialMovingAverage(0.9999, global_step)
+        variables_averages_op = variable_averages.apply(tf.trainable_variables())
+        # Group all updates to into a single train op.
+        train_op = tf.group(apply_gradient_op, variables_averages_op)
+        # Build an initialization operation to run below.
         init = tf.global_variables_initializer()
+        # Start running operations on the Graph. allow_soft_placement must be set to
+        # True to build towers on GPU, as some of the ops do not have GPU
+        # implementations.
+        sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                          gpu_options = tf.GPUOptions(allow_growth=True)))
         sess.run(init)
+        # Create a saver.
         saver = tf.train.Saver(tf.global_variables())
-
         if arg_dict['restore_ckpt']:
             variables_to_restore = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
             restorer = tf.train.Saver(variables_to_restore)
             restorer.restore(sess, arg_dict['restore_ckpt'])
             print ('Resume-trained model restored from: %s' % arg_dict['restore_ckpt'])
 
-        tf.train.write_graph(sess.graph.as_graph_def(), '.', os.path.join(prefix,'graph_struct.txt'))
-
         print ("Start to training...")
+        # batch generator
+        _batch_reader = BatchReader(**arg_dict)
+        _batch_generator = _batch_reader.batch_generator()
         start_time = time.time()
         while not _batch_reader.should_stop():
-            with tf.device('/gpu:0'):
-                batch = _batch_generator.next()
-                _, ploss, step, lr = sess.run([train_op, loss, global_step, learning_rate], 
-                                             feed_dict={images: batch[0], point_labels: batch[1]})
-                if step % 10 == 0:
-                    end_time = time.time()
-                    cost_time, start_time = end_time - start_time, end_time
-                    sample_per_sec = int(10 * batch_size / cost_time)
-                    sec_per_step = cost_time / 10.0
-                    print ('[%s] epochs: %d, step: %d, lr: %f, landmark_loss: %.4f, sample/s: %d, sec/step: %.3f' % (
-                           datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), 
-                           _batch_reader.get_epoch(), step, lr, ploss, sample_per_sec, sec_per_step))
-            if step % 1024 == 0:
+            batch = _batch_generator.next()
+            _, _loss, _step, _lr = sess.run([train_op, loss, global_step, lr], 
+                                         feed_dict={images: batch[0], labels: batch[1]})
+            if _step % 10 == 0:
+                end_time = time.time()
+                cost_time, start_time = end_time - start_time, end_time
+                sample_per_sec = int(10 * batch_size / cost_time)
+                sec_per_step = cost_time / 10.0
+                print ('[%s] epochs: %d, step: %d, lr: %f, landmark_loss: %.6f, sample/s: %d, sec/step: %.3f' % (
+                       datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), 
+                       _batch_reader.get_epoch(), _step, _lr, _loss, sample_per_sec, sec_per_step))
+            if _step % 1024 == 0:
                 checkpoint_path = os.path.join(prefix, 'model.ckpt')
                 saver.save(sess, checkpoint_path)
                 print ('Saved checkpoint to %s' % checkpoint_path)
@@ -80,7 +143,7 @@ def main():
     parser.add_argument('--input_paths', type=str, nargs='+',
                         default='/world/data-c9/liubofang/dataset_original/CelebA/full_path_zf_bbox_pts.txt')
     parser.add_argument('--working_root', type=str, default='/world/data-c9/liubofang/training/landmarks/celeba')
-    parser.add_argument('--batch_size', type=int, default=512, help="Batch size for training")
+    parser.add_argument('--batch_size', type=int, default=512, help="Batch size for training. Total for all gpus.")
     parser.add_argument('--landmark_type', type=int, default=5, help="The number of points. 5 or 83")
     parser.add_argument('--max_epoch', type=int, default=1000, help="Training will be stoped in this case.")
     parser.add_argument('--img_size', type=int, default=128, help="The size of input for model")
@@ -90,7 +153,7 @@ def main():
     parser.add_argument('--model', type=str, default='fanet8ss_inference', help="Model name. Check models.py")
     parser.add_argument('--restore_ckpt', type=str, help="Resume training from special ckpt.")
     parser.add_argument('--try', type=int, default=0, help="Saving path index")
-    parser.add_argument('--gpu_device', type=str, default='7', help="GPU index")
+    parser.add_argument('--gpu_device', type=str, default='4,5,6,7', help="GPU index. Support Multi GPU. eg: 1,2,3")
     parser.add_argument('--img_format', type=str, default='RGB', help="The color format for training.")
     parser.add_argument('--buffer2memory', type=bool, default=False, 
                         help="Read all image to memory to speed up training. Make sure enough memory in your device.")
